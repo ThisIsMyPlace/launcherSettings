fdec2fcec21a616eefd83dc034cd69156be6516a
ThisIsMyPlace <thisismyplace@hushmail.me>
2014-04-23 17:42:09 +0100

    Allow multiple apps to be hidden

--- src/org/thisismyplace/launcherSettings/XposedHook.java
+++ src/org/thisismyplace/launcherSettings/XposedHook.java
@@ -4,7 +4,6 @@ import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
 
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XC_MethodHook;
-import de.robv.android.xposed.XposedBridge;
 import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
 
 public class XposedHook implements IXposedHookLoadPackage {
@@ -13,22 +12,23 @@ public class XposedHook implements IXposedHookLoadPackage {
 	public void handleLoadPackage(LoadPackageParam lpparam) throws Throwable {
 		if(!lpparam.packageName.equals("com.android.launcher3"))
 			return;
-			
-		findAndHookMethod("com.android.launcher3.AllAppsList", lpparam.classLoader, "add", "com.android.launcher3.AppInfo", new XC_MethodHook() {
+
+		final String[] hiddenApps = {"AdAway", "Clock", "Dev Tools", "Downloads", "DSP Manager",
+				"Hacker's Keyboard", "magicPasswd", "Minuum Settings", "Movie Studio",
+				"OmniSwitch", "Search", "Text Edit", "Voice Dialler"};	
+		
+		findAndHookMethod("com.android.launcher3.AllAppsList", lpparam.classLoader,
+				"add", "com.android.launcher3.AppInfo", new XC_MethodHook() {
 			@Override
             protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
 				Object appInfo = param.args[0];
-				String appInfoString = "" + appInfo;//GHETTO, Need to 
+				String appInfoString = "" + appInfo;//GHETTO, Need to fix 
 				String appTitle = appInfoString.substring(22, appInfoString.indexOf(" id"));
-				if(appTitle.equals("AdAway")) {
-					XposedBridge.log(param.method.getName());
-					param.setResult(null);
+				for(int i = 0; i < hiddenApps.length; i++) {
+					if(appTitle.equals(hiddenApps[i]))
+							param.setResult(null);//Stops the procedure being called
 				}
             }
-			@Override
-            protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-				
-			}
 		});
 	}
 
