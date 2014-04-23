70a3482d025b525ca0af99758fe0b6d4591c9726
ThisIsMyPlace <thisismyplace@hushmail.me>
2014-04-23 23:09:55 +0100

    Trebuchet support

--- src/org/thisismyplace/launcherSettings/XposedHook.java
+++ src/org/thisismyplace/launcherSettings/XposedHook.java
@@ -1,9 +1,9 @@
 package org.thisismyplace.launcherSettings;
 
 import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
-
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.XposedBridge;
 import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
 
 public class XposedHook implements IXposedHookLoadPackage {
@@ -20,7 +20,7 @@ public class XposedHook implements IXposedHookLoadPackage {
 				@Override
 				protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
 					Object appInfo = param.args[0];
-					String appInfoString = "" + appInfo;//GHETTO, Need to fix 
+					String appInfoString = "" + appInfo;//GHETTO, Need to fix
 					String appTitle = appInfoString.substring(22, appInfoString.indexOf(" id"));
 					for(int i = 0; i < hiddenApps.length; i++) {
 						if(appTitle.equals(hiddenApps[i]))
@@ -28,14 +28,15 @@ public class XposedHook implements IXposedHookLoadPackage {
 					}
             	}
 			});
-		} else if(lpparam.packageName.equals("com.android.launcher2")) {
-			findAndHookMethod("com.android.launcher2.AllAppsList", lpparam.classLoader,
-					"add", "com.android.launcher2.ApplicationInfo", new XC_MethodHook() {
+		} else if(lpparam.packageName.equals("com.android.launcher2") ||
+				lpparam.packageName.equals("com.cyanogenmod.trebuchet")) {
+			findAndHookMethod(lpparam.packageName + ".AllAppsList", lpparam.classLoader,
+					"add", lpparam.packageName + ".ApplicationInfo", new XC_MethodHook() {
 				@Override
 				protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
 					Object appInfo = param.args[0];
-					String appInfoString = "" + appInfo;//GHETTO, Need to fix 
-					String appTitle = appInfoString.substring(22, appInfoString.indexOf(" id"));
+					String appInfoString = "" + appInfo;//GHETTO, Need to fix
+					String appTitle = appInfoString.substring(22, appInfoString.indexOf(")"));
 					for(int i = 0; i < hiddenApps.length; i++) {
 						if(appTitle.equals(hiddenApps[i]))
 							param.setResult(null);//Stops the procedure being called
